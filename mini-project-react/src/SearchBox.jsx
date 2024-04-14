e4226838350c6d921656d10f75c9f3d93d8d4a5a
Anish Dubey <anishdubey0712@gmail.com>
2024-04-15 03:21:23 +0530

    Added api  by which we can get data

--- mini-project-react/src/SearchBox.jsx
+++ mini-project-react/src/SearchBox.jsx
@@ -3,13 +3,30 @@ import Button from '@mui/material/Button';
 import "./SearchBox.css"
 import { useState } from 'react';
 export default function SearchBox(){
-    let [city ,setCity]= useState("")
+    
+    let [city ,setCity]= useState("");
+    const API_URL = "https://api.openweathermap.org/data/2.5/weather"
+    const API_KEY = "4ffed90bd3b4d4225cc382b12969f59b"
+    let getWeatherInfo = async()=>{
+     let response =    await fetch (`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+    let jsonResponse =  await response.json()
+    console.log(jsonResponse);
+    let result = {
+        temp: jsonResponse.main.temp,
+        tempMin: jsonResponse.main.temp_min,
+        tempMax: jsonResponse.main.temp_max,
+        humidity: jsonResponse.main.humidity,
+        weather: jsonResponse.weather[0].description,
+    }
+    console.log(result)
+    }
     let handleChange = (event)=>{
         setCity(event.target.value)
     }
     let handleSubmit = (event)=>{
         event.preventDefault();//preventDefault is used to prevent default functioning of event like page reload
-        setCity("")
+        setCity("");
+        getWeatherInfo();
     }
     return(
         <div className='SearchBox'>
