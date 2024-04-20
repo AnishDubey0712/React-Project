28dc8b1b949bd1d69734311f4516d6b7a40ef412
Anish Dubey <anishdubey0712@gmail.com>
2024-04-20 17:26:51 +0530

    New File which will display all info

--- mini-project-react/src/InfoBox.jsx
+++ mini-project-react/src/InfoBox.jsx
@@ -4,11 +4,13 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 import './InfoBox.css';
-
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
 export default function InfoBox({info}){
-    const IMG_URL = "https://plus.unsplash.com/premium_photo-1661897016268-b77ad5186d02?q=80&w=1955&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    
-
+    const RAIN_URL = "https://media.istockphoto.com/id/1409305530/photo/monsoon-season-india.webp?b=1&s=170667a&w=0&k=20&c=3Jd5JlkTv-P6a5VT3lfdBiOMzGrCEDddvlXyQXvMHjc="
+    const HOT_URL = "https://media.istockphoto.com/id/453477435/photo/hot.webp?b=1&s=170667a&w=0&k=20&c=Vg9Ibca6xS_APaHriWj_Wko9Pqn9zq1peyEiGnUx3Yg="
+   const COLD_URL = "https://images.unsplash.com/photo-1608259158285-3ea03e3105d4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c3VtbWVyJTIwc2Vhc29ufGVufDB8fDB8fHww"
     return(
         <div className="InfoBox">
           <h1>Weather Info In {info.name}</h1>
@@ -18,12 +20,13 @@ export default function InfoBox({info}){
         <CardMedia
           component="img"
           height="140"
-          image={IMG_URL}
+          image={info.humidity > 80 ? RAIN_URL : info.temp > 15 ? HOT_URL :  COLD_URL}
           alt="green iguana"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {info.name}
+            {info.humidity > 80 ? <ThunderstormIcon/> : info.temp > 15 ? <WbSunnyIcon/>:<AcUnitIcon/> }
           </Typography>
           <Typography variant="body2" color="text.secondary">
             <div>Temperature: {info.temp}&deg;C</div>
