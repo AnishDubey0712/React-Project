6e18a918feabe27f8651ec39025e35dd830b6d52
Anish Dubey <anishdubey0712@gmail.com>
2024-06-29 14:49:31 +0530

    Change

--- mini-project-react/vite.config.js
+++ mini-project-react/vite.config.js
@@ -1,7 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure the output directory is set correctly
+    sourcemap: true,
+  },
+  base: './', // Ensure the base path is set correctly for relative URLs
 })
